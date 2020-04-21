ec29301dde9f8f890dadf14dde23b0bafde8d607
Yushan <yhe10@uncc.edu>
2020-04-21 18:23:11 -0400

    First Commit

---
+++ FPSBreakOut/Assets/Scenes/Player/Player_Camera.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Camera : MonoBehaviour
+{
+    [SerializeField] private string mouseXInputName, mouseYInputName;
+    [SerializeField] private float mouseSensitivity;
+
+    [SerializeField] private Transform playerBody;
+
+    private float xAxisClamp;
+
+    GameObject prefab;//
+
+    private void Awake()
+    {
+        LockCursor();
+        xAxisClamp = 0.0f;
+        prefab = Resources.Load("Sphere") as GameObject;//
+    }
+
+
+    private void LockCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        CameraRotation();
+        
+    }
+
+    private void CameraRotation()
+    {
+        float mouseX = Input.GetAxis(mouseXInputName) * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis(mouseYInputName) * mouseSensitivity * Time.deltaTime;
+
+        xAxisClamp += mouseY;
+
+        if(xAxisClamp > 90.0f)
+        {
+            xAxisClamp = 90.0f;
+            mouseY = 0.0f;
+            ClampXAxisRotationToValue(270.0f);
+        }
+        else if (xAxisClamp < -90.0f)
+        {
+            xAxisClamp = -90.0f;
+            mouseY = 0.0f;
+            ClampXAxisRotationToValue(90.0f);
+        }
+
+        transform.Rotate(Vector3.left * mouseY);
+        playerBody.Rotate(Vector3.up * mouseX);
+        if(Input.GetMouseButtonDown(0))//
+        {
+            GameObject projectile = Instantiate(prefab) as GameObject;
+            projectile.transform.position = transform.forward * 2;
+            Rigidbody rb = projectile.GetComponent<Rigidbody>();
+            rb.velocity = transform.forward*40;
+        }
+    }
+
+    private void ClampXAxisRotationToValue(float value)
+    {
+        Vector3 eulerRotation = transform.eulerAngles;
+        eulerRotation.x = value;
+        transform.eulerAngles = eulerRotation;
+    }
+}
