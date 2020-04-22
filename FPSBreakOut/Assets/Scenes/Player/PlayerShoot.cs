e977657961c8e52d119f0df3bc6bacacadb35c30
Axel-KM <48526747+Axel-KM@users.noreply.github.com>
2020-04-21 22:54:11 -0400

    test

---
+++ FPSBreakOut/Assets/Scenes/Player/PlayerShoot.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    GameObject prefab;
+    [SerializeField] private Transform playerBody;
+
+    void Awake()
+    {
+        prefab = Resources.Load("Ball/ball") as GameObject;//easy load for projectiles
+    }
+
+    void Update()
+    {
+        Shoot();
+    }
+
+    private void Shoot()
+    {
+        if (Input.GetMouseButtonDown(0))// makes balls
+        {
+            GameObject projectile = Instantiate(prefab) as GameObject;
+            projectile.transform.position = playerBody.position + transform.forward * 2;
+            Rigidbody rb = projectile.GetComponent<Rigidbody>();
+            rb.velocity = transform.forward * 40;
+        }
+    }
+}
---
+++ FPSBreakOut/Assets/Scenes/Resources/Ball/ball.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private float destroyTime = 5;
+
+    void Start()
+    {
+        //trigger ball popping animation after a time delay
+        PopBall();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "box")
+        {
+            //Debug.Log(col.gameObject.name);
+            PopBall(true);
+        }
+    }
+
+    //destroys ball after a set time or collision with boxes
+    void PopBall(bool PopNow=false)
+    {
+        if (PopNow)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject, destroyTime);
+        }
+
+    }
+}
