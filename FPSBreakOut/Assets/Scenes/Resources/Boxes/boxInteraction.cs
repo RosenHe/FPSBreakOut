56faa6363670a2fba909b038a2ce2abb6b9c9a99
Axel-KM <48526747+Axel-KM@users.noreply.github.com>
2020-04-26 19:27:47 -0400

    added zipped project

--- FPSBreakOut/Assets/Scenes/Resources/Boxes/boxInteraction.cs
+++ FPSBreakOut/Assets/Scenes/Resources/Boxes/boxInteraction.cs
@@ -15,13 +15,13 @@ public class boxInteraction : MonoBehaviour
     }
     void Start()
     {
- 
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnCollisionEnter(Collision col)
@@ -30,17 +30,17 @@ public class boxInteraction : MonoBehaviour
 
         Transform target = col.gameObject.GetComponentInParent<Transform>();
 
-        if (col.gameObject.tag == "ball" && transform.parent != null)
+        if (col.gameObject.tag == "ball" && transform.parent != null) //first ball hit
         {
             boxBody.isKinematic = false;
             Debug.Log(col.gameObject.name);
             transform.parent = null;
             Vector3 dir = new Vector3(0, .1f, -1);
             boxBody.useGravity = true;
-            boxBody.AddForce(dir*force, ForceMode.VelocityChange);
+            boxBody.AddForce(dir * force, ForceMode.VelocityChange);
         }
-
-        if (col.gameObject.tag == "Player" && boxBody.isKinematic == false)
+        else if (col.gameObject.tag == "ball" && transform.parent == null ||    //second ball hit
+                 col.gameObject.tag == "Player" && boxBody.isKinematic == false)//or touches player
         {
             boxBody.AddForce(Vector3.down * force);
         }
